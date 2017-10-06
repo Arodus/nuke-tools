feb02f5603d9778ef0b92e24e7c25a82e058068f
Arodus <mail@sebastian-karasek.de>
2017-10-06 22:00:24 +0200

    Add Target to update and commit updated references

--- build/Build.cs
+++ build/Build.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Linq;
+using Nuke.Common.Tools;
 using Nuke.Core;
+using Nuke.Core.Tooling;
 using static Nuke.Core.IO.FileSystemTasks;
 using static Nuke.Core.IO.PathConstruction;
 using static ReferenceDownload;
 
 class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.References);
+    public static int Main() => Execute<Build>(x => x.Commit);
 
     string MetadataDirectory => SolutionDirectory / "metadata";
     string ReferencesDirectory => (AbsolutePath) MetadataDirectory / "references";
 
     Target Clean => _ => _
-            .Executes(() => EnsureCleanDirectory(ReferencesDirectory));
+        .Executes(() => EnsureCleanDirectory(ReferencesDirectory));
 
     Target References => _ => _
-            .DependsOn(Clean)
-            .Executes(() => DownloadReferences(MetadataDirectory, ReferencesDirectory));
+        .DependsOn(Clean)
+        .Executes(() => DownloadReferences(MetadataDirectory, ReferencesDirectory));
+
+
+   
+    Target Commit => _ => _
+        .DependsOn(Clean)
+        .Executes(() => ProcessTasks.StartProcess(ToolPathResolver.GetPathExecutable("git"), "pull origin master").AssertZeroExitCode())
+        .Executes(References)
+        .Executes(() => GitCommit.CommitIfChanged("Update references.", ReferencesDirectory))
+        .Executes(() => ProcessTasks.StartProcess(ToolPathResolver.GetPathExecutable("git"), "push origin").AssertZeroExitCode());
 }
